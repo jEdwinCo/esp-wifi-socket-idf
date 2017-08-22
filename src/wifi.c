81b5e3f9cb321f91b294c7dec62c7acc2b757fc3
Jhon Edwin Cordoba <edwin657@gmail.com>
2017-08-22 16:06:40 -0300

    include wifi station file

---
+++ src/wifi.c
@@ -0,0 +1,82 @@
+/*
+* source based in
+https://github.com/espressif/esp-idf/blob/master/examples/performance/tcp_perf/main/tcp_perf.h
+*/
+#include "wifi.h"
+#include <string.h>
+#include <sys/socket.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+
+#define EXAMPLE_DEFAULT_SSID CONFIG_WIFI_SSID
+#define EXAMPLE_DEFAULT_PWD  CONFIG_WIFI_PASSWORD
+#define TAG "tcp_perf:"
+#define WIFI_CONNECTED_BIT BIT0
+
+/* FreeRTOS event group to signal when we are connected to wifi */
+EventGroupHandle_t tcp_event_group;
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    switch (event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        esp_wifi_connect();
+        xEventGroupClearBits(tcp_event_group, WIFI_CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_STA_CONNECTED:
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        ESP_LOGI(TAG, "got ip:%s\n",
+                 ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+        xEventGroupSetBits(tcp_event_group, WIFI_CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_AP_STACONNECTED:
+        ESP_LOGI(TAG, "station:"MACSTR" join,AID=%d\n",
+                 MAC2STR(event->event_info.sta_connected.mac),
+                 event->event_info.sta_connected.aid);
+        xEventGroupSetBits(tcp_event_group, WIFI_CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_AP_STADISCONNECTED:
+        ESP_LOGI(TAG, "station:"MACSTR"leave,AID=%d\n",
+                 MAC2STR(event->event_info.sta_disconnected.mac),
+                 event->event_info.sta_disconnected.aid);
+        xEventGroupClearBits(tcp_event_group, WIFI_CONNECTED_BIT);
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+//wifi_init_sta
+void wifi_init_sta()
+{
+    tcp_event_group = xEventGroupCreate();
+
+    //tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_DEFAULT_SSID,
+            .password = EXAMPLE_DEFAULT_PWD
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s \n",
+             EXAMPLE_DEFAULT_SSID, EXAMPLE_DEFAULT_PWD);
+}
---
+++ src/wifi.h
@@ -0,0 +1,7 @@
+#ifndef __WIFI_H__
+#define __WIFI_H__
+
+//using esp as station
+void wifi_init_sta();
+
+#endif /**/
